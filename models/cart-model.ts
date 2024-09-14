6bd05d647dff17fa129941edb5e9e0de00d3ca3b
jakia911 <gazijakia911@gmail.com>
2024-09-14 20:00:09 +0600

    cart model added

---
+++ models/cart-model.ts
@@ -0,0 +1,30 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+// Define the item structure in the cart
+interface CartItem {
+  productId: mongoose.Schema.Types.ObjectId;
+  quantity: number;
+  price: number;
+  image:string
+}
+
+// Define the Cart interface
+export interface ICart extends Document {
+  user: mongoose.Schema.Types.ObjectId;
+  items: CartItem[];
+}
+
+// Create the schema
+const cartSchema: Schema<ICart> = new Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  items: [
+    {
+      productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+      quantity: { type: Number, required: true },
+      price: { type: Number, required: true },
+      image: { type: Number, required: true },
+    },
+  ],
+});
+
+export const cartModel = mongoose.model<ICart>("Cart", cartSchema);
