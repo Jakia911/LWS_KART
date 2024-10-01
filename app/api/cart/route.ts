acbf48c83f48f768cfe6cd91d7fe76d564deb675
jakia911 <gazijakia911@gmail.com>
2024-10-01 19:44:27 +0600

    update cart data updated

--- app/api/cart/route.ts
+++ app/api/cart/route.ts
@@ -1,15 +1,10 @@
 
-import { cartModel } from "@/models/cart-model";
+import { cartModel, ICart } from "@/models/cart-model";
 import { dbConnect } from "@/services/mongo";
+import { CartRequestBody, UpdateCartQuantityRequest } from "@/types/cart";
 import { NextResponse } from "next/server";
 
-interface CartRequestBody{
-  userName:string,
-  productId:string,
-   name:string,
-  price: number;
-  image: string;
-}
+
 
 // for post data
 export const POST = async(request:Request):Promise<NextResponse> => {
@@ -51,4 +46,52 @@ export const POST = async(request:Request):Promise<NextResponse> => {
 
 // for updating data
 
-// export const PUT = async()
+
+
+
+
+
+export const PUT = async (request: Request): Promise<NextResponse> => {
+  const { productId, quantity, userName }: UpdateCartQuantityRequest = await request.json();
+
+  await dbConnect();
+
+ try {
+   
+    const existingCart = await cartModel.findOne({ userName }).lean<ICart>(); 
+    console.log('Existing Cart:', existingCart);
+
+    // If no cart is found, return a 404 response
+    if (!existingCart) {
+      return NextResponse.json({ message: 'Cart not found' }, { status: 404 });
+    }
+
+    // Find the product in the cart by productId
+    // const productIndex = existingCart.items.findIndex((item) => item.productId === productId);
+
+    // if (productIndex === -1) {
+    //   return NextResponse.json({ message: 'Product not found in cart' }, { status: 404 });
+    // }
+
+    // Update the quantity of the product
+    // existingCart.items[productIndex].quantity = quantity;
+
+ 
+    await cartModel.updateOne(
+      { userName, 'items.productId': productId },  // Find the specific cart and product
+      { $set: { 'items.$.quantity': quantity } }   // Use the positional operator $ to update the correct product
+    );
+
+    
+    return NextResponse.json({ message: 'Product quantity updated successfully' }, { status: 200 });
+
+  } catch (error: any) {
+    
+    console.error('Error updating cart:', error);
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
+  
+};
+
+
+
--- types/cart.ts
+++ types/cart.ts
@@ -1,2 +1,14 @@
 
 
+export interface UpdateCartQuantityRequest {
+  productId: string;
+  quantity: number;
+  userName: string;
+}
+export interface CartRequestBody{
+  userName:string,
+  productId:string,
+   name:string,
+  price: number;
+  image: string;
+}
