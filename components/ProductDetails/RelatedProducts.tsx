96f56270f1be897b29c69656d16cb5be7cd2ea17
jakia911 <gazijakia911@gmail.com>
2024-10-09 21:01:24 +0600

    related products card component added

---
+++ components/ProductDetails/RelatedProducts.tsx
@@ -0,0 +1,12 @@
+const RelatedProducts = () => {
+  return (
+    <>
+      <h2 className="text-2xl font-medium text-gray-800 uppercase mb-6">
+        Related Products
+      </h2>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-6"></div>
+    </>
+  );
+};
+
+export default RelatedProducts;
