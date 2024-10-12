2718c94ba59b96256b1f4fd31a166260792882d6
jakia911 <gazijakia911@gmail.com>
2024-10-12 12:49:58 +0600

    handler function added for search form

--- components/header/SearchForm.tsx
+++ components/header/SearchForm.tsx
@@ -1,19 +1,39 @@
+import { useRouter } from "next/router";
+import { ChangeEvent, useState } from "react";
+
 const SearchForm = () => {
+  const [searchQuery, setSearchQuery] = useState<string>(""); // Specify that searchQuery is of type string
+  const router = useRouter();
+
+  const handleSearchSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (searchQuery.trim()) {
+      router.push(`/shop?search=${encodeURIComponent(searchQuery)}`);
+    }
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
   return (
     <div className="w-full max-w-xl relative flex">
-      <span className="absolute left-4 top-3 text-lg text-gray-400">
-        <i className="fa-solid fa-magnifying-glass"></i>
-      </span>
-      <input
-        type="text"
-        name="search"
-        id="search"
-        className="w-full border border-primary border-r-0 pl-12 py-3 pr-3 rounded-l-md focus:outline-none hidden md:flex"
-        placeholder="search"
-      />
-      <button className="bg-primary border border-primary text-white px-8 rounded-r-md hover:bg-transparent hover:text-primary transition hidden md:flex">
-        Search
-      </button>
+      <form action="" onSubmit={handleSearchSubmit}>
+        <span className="absolute left-4 top-3 text-lg text-gray-400">
+          <i className="fa-solid fa-magnifying-glass"></i>
+        </span>
+        <input
+          type="text"
+          name="search"
+          id="search"
+          onChange={handleChange}
+          className="w-full border border-primary border-r-0 pl-12 py-3 pr-3 rounded-l-md focus:outline-none hidden md:flex"
+          placeholder="search"
+        />
+        <button className="bg-primary border border-primary text-white px-8 rounded-r-md hover:bg-transparent hover:text-primary transition hidden md:flex">
+          Search
+        </button>
+      </form>
     </div>
   );
 };
