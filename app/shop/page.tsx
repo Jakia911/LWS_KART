d7297b3443a75877b9dd4a404bcde5bb4678e702
jakia911 <gazijakia911@gmail.com>
2024-10-16 23:41:40 +0600

    price filter component created

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import CategoryFilter from "@/components/shop/CategoryFilter";
+import PriceFilter from "@/components/shop/PriceFilter";
 import ShopProducts from "@/components/ShopProducts";
 import { Product } from "@/types/product";
 import { useEffect, useState } from "react";
@@ -335,34 +336,14 @@ const ShopPage = () => {
 
         <div className="col-span-1 bg-white px-4 pb-6 shadow rounded overflow-hiddenb hidden md:block">
           <div className="divide-y divide-gray-200 space-y-5">
+            {/* category filter  */}
             <CategoryFilter
               selectedCategories={selectedCategories}
               products={products}
               handleCategoryChange={handleCategoryChange}
             />
-
-            <div className="pt-4">
-              <h3 className="text-xl text-gray-800 mb-3 uppercase font-medium">
-                Price
-              </h3>
-              <div className="mt-4 flex items-center">
-                <input
-                  type="text"
-                  name="min"
-                  id="min"
-                  className="w-full border-gray-300 focus:border-primary rounded focus:ring-0 px-3 py-1 text-gray-600 shadow-sm"
-                  placeholder="min"
-                />
-                <span className="mx-3 text-gray-500">-</span>
-                <input
-                  type="text"
-                  name="max"
-                  id="max"
-                  className="w-full border-gray-300 focus:border-primary rounded focus:ring-0 px-3 py-1 text-gray-600 shadow-sm"
-                  placeholder="max"
-                />
-              </div>
-            </div>
+            {/* price filter  */}
+            <PriceFilter />
 
             <div className="pt-4">
               <h3 className="text-xl text-gray-800 mb-3 uppercase font-medium">
