3c0df5f20bec841252f30e6202987aba1fbebe39
jakia911 <gazijakia911@gmail.com>
2024-10-27 15:06:28 +0600

    top trending data fetched successfull

--- components/trendingProducts/TrendingProducts.tsx
+++ components/trendingProducts/TrendingProducts.tsx
@@ -1,20 +1,35 @@
-import { getServerSession } from "next-auth";
-import { getAllProducts } from "../../database/queries/index";
+"use client";
 
-import { authOptions } from "@/auth";
+import { Product } from "@/types/product";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 import TrendingProductCard from "./TrendingProductCard";
 
-const TrendingProducts = async () => {
-  const products = await getAllProducts();
+const TrendingProducts = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
-  const trendingProducts = products.slice(0, 6);
-  console.log(products);
+  const { data: session, status } = useSession();
 
-  const session = await getServerSession(authOptions);
-  console.log("trending product data", session?.user);
-  const userName: string | null | undefined = session?.user?.name;
+  const userName: string | undefined | null = session?.user?.name;
+  const fetchProducts = () => {
+    setLoading(true);
 
-  // Define the expected structure of the request body
+    fetch("/api/products/trending")
+      .then((response) => {
+        if (!response.ok) throw new Error("Failed to fetch products");
+        return response.json();
+      })
+      .then((data) => setProducts(data))
+      .catch((error) => setError(error.message))
+      .finally(() => setLoading(false));
+  };
+
+  // Fetch sorted products on component mount
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
   return (
     <div className="container pb-16">
@@ -22,7 +37,7 @@ const TrendingProducts = async () => {
         TRENDING PRODUCTS
       </h2>
       <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
-        {trendingProducts.map((prod) => (
+        {products.map((prod) => (
           <TrendingProductCard prod={prod} userName={userName} />
         ))}
       </div>
